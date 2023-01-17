4899d7cd35f8313465ae16dd7575718c0da4711c
Ramon <ramon.mitra81@gmail.com>
2023-01-17 10:10:25 +0100

    ...

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import "../src/Styles/App.css";
 import Layout from "../src/Layout/Layout";
 import routes from "../src/Navigation/routes";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Layout>
         <Routes>
           {routes.map((route) => {
@@ -13,7 +13,7 @@ function App() {
           })}
         </Routes>
       </Layout>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
--- src/Navigation/routes.jsx
+++ src/Navigation/routes.jsx
@@ -6,12 +6,12 @@ import Contact from "../Pages/Contact/Contact";
 import CharSheet from "../Pages/CharSheet/CharSheet";
 
 const routes = [
-  { path: "./", element: <HomePage />, id: 1 },
-  { path: "./about-team", element: <AboutTeam />, id: 2 },
-  { path: "./impressum", element: <Impressum />, id: 3 },
+  { path: "/", element: <HomePage />, id: 1 },
+  { path: "/about-team", element: <AboutTeam />, id: 2 },
+  { path: "/impressum", element: <Impressum />, id: 3 },
   { path: "*", element: <NotFound />, id: 4 },
-  { path: "./contact", element: <Contact />, id: 5 },
-  { path: "./char-sheet", element: <CharSheet />, id: 6 },
+  { path: "/contact", element: <Contact />, id: 5 },
+  { path: "/char-sheet", element: <CharSheet />, id: 6 },
 ];
 
 export default routes;
